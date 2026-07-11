5ab1710b1ee7021d7a0ce07e614b326327a5bde2
Karthikgs3 <karthikgsharma043@gmail.com>
2026-07-11 15:32:24 +0530

    Temporarily hardcode CORS origins for debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_ORIGIN],
+    allow_origins=["http://localhost:3000",
+    "https://stockpilot-ai-nine.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
