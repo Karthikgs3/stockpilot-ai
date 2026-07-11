1fe8b35138ad64a14e48e9cc216a4409580ff13e
Karthikgs3 <karthikgsharma043@gmail.com>
2026-07-11 16:12:46 +0530

    main.py update

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,8 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-    "https://stockpilot-ai-nine.vercel.app",],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
